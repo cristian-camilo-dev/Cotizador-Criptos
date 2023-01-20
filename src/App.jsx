cac9587252228ed489439bc83f73e2b9ff78f275
Cristian Rodriguez <cristianrodrig149@gmail.com>
2023-01-20 03:01:51 +0100

    ✨feat: add styleComponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+import ImgCripto from "../src/assets/imagen-criptos.png";
 
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 50px;
 
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+    
+  }
+`;
+
+const Container = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+}
+`;
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+  background: rgba( 1, 29, 41, 0.75 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 4.5px );
+-webkit-backdrop-filter: blur( 4.5px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );
+`;
 
 function App() {
-  
-
   return (
-    <h1>Desde app</h1>
-  )
+    <Container>
+      <Image src={ImgCripto} alt="Imagen criptomonedas" />
+      <Heading>Desde app</Heading>;
+    </Container>
+  );
 }
 
-export default App
+export default App;
